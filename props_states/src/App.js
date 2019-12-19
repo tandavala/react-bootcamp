7654e4ebad293abc65b59a56f970258355af0b5a
tandavala <jose.tandavala@gmail.com>
2019-12-19 08:00:48 +0100

    working with props

--- props_states/src/App.js
+++ props_states/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <Parent />;
+  }
 }
 
+class Parent extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Just some info</h2>
+        <Cars msg="cars are cool" model="3245" coolCars={this.props.cars} />
+      </div>
+    );
+  }
+}
+Parent.defaultProps = {
+  cars: ["BMW", "MERC", "City", "Audi"]
+};
+class Cars extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <h3>I am from cars component</h3>
+        <p>{this.props.msg}</p>
+        <p>{this.props.model}</p>
+
+        {this.props.coolCars.map((item, i) => {
+          return item + " ";
+        })}
+      </div>
+    );
+  }
+}
 export default App;
